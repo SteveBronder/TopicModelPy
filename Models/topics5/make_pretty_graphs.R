948921e42672a0c2e5789a63b1bbd540d0f78b2b
Steve <stevo15025@gmail.com>
2016-05-17 03:32:34 -0400

    Make final changes to docs and example lda model.

--- Models/topics5/make_pretty_graphs.R
+++ Models/topics5/make_pretty_graphs.R
@@ -1,23 +1,52 @@
 library(ggplot2)
 library(ggthemes)
+library(xtable)
 library(coda)
+library(data.table)
 
+citations <- read.csv("./citations.csv")
 likelihood <- read.csv("./likelihood.csv")
 dtm_dat <- read.csv("./dtm_dat.csv")
 phi <- read.csv("./phi_new.csv")
 phi_old <- read.csv("./phi.csv")
-# Fix rounding error
-phi <- phi[,-1]/apply(phi[,-1],1,sum)
-phi_old <- phi_old[,-1]/apply(phi_old[,-1],2,sum)
-
 theta <- read.csv("./theta.csv")
 doc_length <- apply(dtm_dat[,-1],1,sum)
 vocab <- colnames(phi)[-1]
 term_frequency <- apply(dtm_dat[,-1],2,sum)
+
+# Fix rounding error
+phi <- phi[,-1]/apply(phi[,-1],1,sum)
+phi_old <- phi_old[,-1]/apply(phi_old[,-1],2,sum)
+
+#make and merge citations
+citations[,2]<-as.character(citations[,2])
+colnames(citations)[2] <- "source"
+dtm_dat[,1] <- as.character(dtm_dat[,1])
+dtm_dat[,1] <- substr(dtm_dat[,1],35,49)
+dtm_dat[,1] <- gsub("_","/",dtm_dat[,1])
+
+dtm_dat2 <- data.table(dtm_dat)
+citations2 <- data.table(citations)
+setkey(dtm_dat2,"source")
+setkey(citations2,"source")
+dtm_cit<-citations2[,c("source","title"),with=FALSE][dtm_dat2]
+theta$X <- dtm_cit[,"title",with=FALSE]
+
 ggplot(likelihood, aes(x=X, y=X0)) + geom_line() + theme_tufte() + xlab("Iterations") + ylab("Likelihood")
 
+theta_top <- list()
+for (i in 2:6){
+  theta_test <- theta[order(theta[,i],decreasing=TRUE),"X"]
+  theta_test <- as.data.frame(head(theta_test[title != "",],n=10))
+  theta_top[I(i-1)] <- data.frame("topic_1" = as.character(theta_test$title),stringsAsFactors=FALSE)
+}
+lapply(1:5,function(i) (xtable(data.frame(theta_top[[i]]))))
+theta_top <-do.call(cbind,theta_top)
+
+xtable(theta_top)
 # Geweke score
 geweke.diag(mcmc(likelihood$X0))
+heidel.diag(mcmc(likelihood$X0))
 
 t_phio <- t(phi_old)
 
@@ -30,6 +59,8 @@ phi_top <- lapply(phi_top, as.character)
 phi_top <- do.call(cbind,phi_top)
 phi_top <- as.data.frame(phi_top)
 colnames(phi_top) <- paste0("topic_",1:5)
+
+xtable(phi_top)
 
 # Fancy Graph
 ##########################################
